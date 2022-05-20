82f2dc74f99e351ab665b917afb40a27db86b0bb
BrunoSouzza <souza_0010@hotmail.com>
2022-05-20 11:14:14 -0300

    refectoring

---
+++ AzureAPI/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AzureAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly List<User> _user;
+
+
+        public UserController()
+        {
+            _user = new List<User>
+            {
+                new User { Id = 1 ,Name = "Bruno"},
+                new User { Id = 2 ,Name = "Carlo"},
+                new User { Id = 3 ,Name = "Paula"}
+            };
+        }
+
+        [HttpGet("all")]
+        public IActionResult GetAll()
+        {
+            return Ok(_user);
+        }
+    }
+
+    class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
